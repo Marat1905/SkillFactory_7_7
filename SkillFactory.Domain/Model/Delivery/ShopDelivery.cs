105cf53de715a5d068aa8c3b1589137a05d4d3bd
Marat <milya010790@mail.ru>
2023-12-19 20:03:10 +0500

    Доставка в розничный магазин

---
+++ SkillFactory.Domain/Model/Delivery/ShopDelivery.cs
@@ -0,0 +1,25 @@
+﻿using SkillFactory.Domain.Model.Base;
+
+namespace SkillFactory.Domain.Model
+{
+    /// <summary>Доставка в розничный магазин</summary>
+    public class ShopDelivery : Delivery
+    {
+        /// <summary>Ф.И.О. клиента</summary>
+        public string ClientName { get; private set; }
+
+        /// <summary>Название магазина</summary>
+        public string ShopName { get; private set; }
+
+        /// <summary><inheritdoc cref="ShopDelivery"/></summary>
+        /// <param name="amount"><inheritdoc cref="Delivery.DeliveryAmount" path="/summary"/></param>
+        /// <param name="adress"><inheritdoc cref="Delivery.Adress" path="/summary"/></param>
+        /// <param name="clientName"><inheritdoc cref="ClientName" path="/summary"/></param>
+        /// <param name="shopName"><inheritdoc cref="ShopName" path="/summary"/></param>
+        public ShopDelivery(int amount, string adress, string clientName, string shopName) : base(amount, adress)
+        {
+            ClientName = clientName;
+            ShopName = shopName;
+        }
+    }
+}
