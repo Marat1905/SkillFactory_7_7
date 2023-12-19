aa576789aa73d184c0823433dca71b3d806bb4d6
Marat <milya010790@mail.ru>
2023-12-19 20:01:23 +0500

    Создали базовый класс для доставок

---
+++ SkillFactory.Domain/Model/Base/Delivery.cs
@@ -0,0 +1,21 @@
+﻿namespace SkillFactory.Domain.Model.Base
+{
+    /// <summary>Доставка</summary>
+    public abstract class Delivery
+    {
+        /// <summary>Сумма доставки</summary>
+        public  int DeliveryAmount { get; protected set; }
+
+        /// <summary>Адрес доставки</summary>
+        public  string Adress { get; protected set; }
+
+        /// <summary><inheritdoc cref="Delivery"/></summary>
+        /// <param name="amount"><inheritdoc cref="Delivery.DeliveryAmount" path="/summary"/></param>
+        /// <param name="adress"><inheritdoc cref="Delivery.Adress" path="/summary"/></param>
+        public Delivery(int amount, string adress)
+        {
+            DeliveryAmount = amount;
+            Adress = adress;
+        }
+    }
+}
