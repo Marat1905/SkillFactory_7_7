7b6ab67ac80f834bc5a372e7c0c49b7cea3d6afa
Marat <milya010790@mail.ru>
2023-12-19 22:42:45 +0500

    Добавлен класс клиента

---
+++ SkillFactory.Domain/Model/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace SkillFactory.Domain.Model
+{
+    /// <summary>Клиент</summary>
+   public class Customer
+    {
+        /// <summary>Имя</summary>
+        public string Name { get; private set; }
+
+        /// <summary>Адрес</summary>
+        public string  Address { get; internal set; }
+
+        /// <summary><inheritdoc cref="Customer"/></summary>
+        /// <param name="name"><inheritdoc cref="Customer.Name" path="/summary"/></param>
+        /// <param name="address"><inheritdoc cref="Customer.Address" path="/summary"/></param>
+        public Customer(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"Имя клиента: {Name} Адрес доставки: {Address}";
+        //}
+    }
+}
