9e898901ac67f7dbd8d647b35ae882f2f1d704ea
Marat <milya010790@mail.ru>
2023-12-19 22:42:13 +0500

    Добавлен класс курьера

---
+++ SkillFactory.Domain/Model/Courier.cs
@@ -0,0 +1,26 @@
+﻿namespace SkillFactory.Domain.Model
+{
+    /// <summary>Курьер</summary>
+   public class Courier
+    {
+        /// <summary>Имя</summary>
+        public readonly string Name;
+
+        /// <summary>Телефон</summary>
+        public readonly string Phone;
+
+        /// <summary><inheritdoc cref="Courier"/></summary>
+        /// <param name="name"><inheritdoc cref="Courier.Name" path="/summary"/></param>
+        /// <param name="phone"><inheritdoc cref="Courier.Phone" path="/summary"/></param>
+        public Courier(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"Имя курьера: {Name} телефон: {Phone}";
+        //}
+    }
+}
