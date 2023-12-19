d0085c92f32db470387a9baf4990ef5b7cab1389
Marat <milya010790@mail.ru>
2023-12-19 22:43:13 +0500

    Добавлен класс магазин

---
+++ SkillFactory.Domain/Model/Shop.cs
@@ -0,0 +1,26 @@
+﻿namespace SkillFactory.Domain.Model
+{
+    /// <summary>Магазин</summary>
+    public class Shop
+    {
+        /// <summary>Имя</summary>
+        public readonly string Name;
+
+        /// <summary>Адрес</summary>
+        public readonly string Address;
+
+        /// <summary><inheritdoc cref="Shop"/></summary>
+        /// <param name="name"><inheritdoc cref="Shop.Name" path="/summary"/></param>
+        /// <param name="address"><inheritdoc cref="Shop.Address" path="/summary"/></param>
+        public Shop(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"Название магазина: {Name} Находится по адресу: {Address}";
+        //}
+    }
+}
