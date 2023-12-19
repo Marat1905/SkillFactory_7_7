5a5e74de08590c01bab132693f4a2b5c3ecdc2b9
Marat <milya010790@mail.ru>
2023-12-19 20:02:50 +0500

    Доставка в пункт выдачи

---
+++ SkillFactory.Domain/Model/Delivery/PickPointDelivery.cs
@@ -0,0 +1,22 @@
+﻿using SkillFactory.Domain.Model.Base;
+
+namespace SkillFactory.Domain.Model
+{
+    /// <summary>Доставка в пункт выдачи </summary>
+    public class PickPointDelivery : Delivery
+    {
+        /// <summary>Ф.И.О. клиента</summary>
+        public string ClientName { get; private set; }
+
+        /// <summary><inheritdoc cref="PickPointDelivery"/></summary>
+        /// <param name="amount"><inheritdoc cref="Delivery.DeliveryAmount" path="/summary"/></param>
+        /// <param name="adress"><inheritdoc cref="Delivery.Adress" path="/summary"/></param>
+        /// <param name="clientName"><inheritdoc cref="ClientName" path="/summary"/></param>
+        public PickPointDelivery(int amount, string adress, string clientName) : base(amount, adress)
+        {
+            ClientName = clientName;
+        }
+
+
+    }
+}
